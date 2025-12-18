737da9518079d73d249a8595fc61df0affba7742
YoelUb <yurqubar@myuax.com>
2025-12-18 09:51:03 +0100

    Puntos 3?

--- app/src/main/java/com/example/parcial_ll_yoel_urquijo_barroso_150739/MainActivity.java
+++ app/src/main/java/com/example/parcial_ll_yoel_urquijo_barroso_150739/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.parcial_ll_yoel_urquijo_barroso_150739;
 
 import android.os.Bundle;
-
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.auth.FirebaseAuth;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etEmail, etPassword;
+    private Button btnRegister;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,46 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(v -> registerUser());
+    }
+
+    private void registerUser() {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Correo inválido");
+            return;
+        }
+
+        if (!isValidPassword(password)) {
+            etPassword.setError("La contraseña no cumple los requisitos.");
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Registro Exitoso", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private boolean isValidPassword(String password) {
+        Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$");
+        return pattern.matcher(password).matches();
     }
 }
